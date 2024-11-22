e65108c0b195bd5596c58a017bf18234e15e8ff7
Nachokeso <167124110+IgnacioBerrios@users.noreply.github.com>
2024-11-22 12:27:18 -0300

    Update pie_cosas.py

--- pie_cosas.py
+++ pie_cosas.py
@@ -6,11 +6,8 @@ file_path = 'spotify_songs_dataset.csv'
 
 df = pd.read_csv(file_path)
 
-contador_lenguaje = df['language'].value_counts()
-
-fig, ax = plt.subplots(figsize=(10, 8))
-ax.pie(contador_lenguaje, labels=contador_lenguaje.index, autopct='%1.1f%%', startangle=140, colors=plt.cm.tab20.colors)
-ax.set_title('Distribución de Canciones por Idioma')
-
-st.pyplot(fig)
-
+contador_lenguaje = pf["language"].value_counts()
+            fig, ax = plt.subplots(figsize=(10, 8))
+            ax.pie(contador_lenguaje, labels=contador_lenguaje.index, autopct='%1.1f%%', startangle=140, colors=plt.cm.tab20.colors)
+            ax.set_title('Distribución de Canciones por Idioma')
+            st.pyplot(fig)
