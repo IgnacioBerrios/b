4ccf765852c747c1c3d9a5bf67616964a15a8fc0
Nachokeso <167124110+IgnacioBerrios@users.noreply.github.com>
2024-11-22 12:24:02 -0300

    Update pie_cosas.py

--- pie_cosas.py
+++ pie_cosas.py
@@ -2,21 +2,15 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import streamlit as st
 
-ruta = 'spotify_songs_dataset.csv'
+file_path = 'spotify_songs_dataset.csv'
 
-df = pd.read_csv(ruta)
+df = pd.read_csv(file_path)
 
-if "language" in df.columns:
-    contador_lenguaje = df["language"].value_counts()
+contador_lenguaje = df["language"].value_counts()
 
-    fig, ax = plt.subplots(figsize=(10, 8))
-    ax.pie(
-        contador_lenguaje, 
-        labels=contador_lenguaje.index, 
-        autopct='%1.1f%%', 
-        startangle=140, 
-        colors=plt.cm.tab20.colors
-    )
-    ax.set_title('Distribución de Canciones por Idioma')
+fig, ax = plt.subplots(figsize=(10, 8))
+ax.pie(contador_lenguaje, labels=contador_lenguaje.index, autopct='%1.1f%%', startangle=140, colors=plt.cm.tab20.colors)
+ax.set_title('Distribución de Canciones por Idioma')
 
-    st.pyplot(fig)
+st.pyplot(fig)
+
